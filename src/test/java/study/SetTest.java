a294b80357fc513f2a49396e5537cbc3d2f9fdd6
Min Heo <min103ju@gmail.com>
2021-05-30 15:54:52 +0900

    학습테스트 set collection 요구사항 2 완료

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -2,6 +2,8 @@ package study;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -25,5 +27,11 @@ public class SetTest {
     void size() {
         int actual = numbers.size();
         assertThat(actual).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
     }
 }
