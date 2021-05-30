645492690d20db8e6a963510904dfa32a4ce83d7
Min Heo <min103ju@gmail.com>
2021-05-30 12:30:09 +0900

    학습테스트 요구사항 2 완료

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -19,4 +19,10 @@ public class StringTest {
         String[] actual2 = "1,".split(",");
         assertThat(actual2).containsExactly("1");
     }
+
+    @Test
+    void substring() {
+        String actual = "(1,2)".substring(1, 4);
+        assertThat(actual).isEqualTo("1,2");
+    }
 }
