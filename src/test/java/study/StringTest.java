b5a9aedb2b1426e489f83bf290e2ca7863cba566
Min Heo <min103ju@gmail.com>
2021-05-30 12:28:40 +0900

    학습테스트 요구사항 1 완료

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,13 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() {
+        String[] actual1 = "1,2".split(",");
+        assertThat(actual1).contains("1","2");
+
+        String[] actual2 = "1,".split(",");
+        assertThat(actual2).containsExactly("1");
+    }
 }
